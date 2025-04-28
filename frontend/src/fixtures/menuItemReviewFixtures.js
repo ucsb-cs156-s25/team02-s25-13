824e6e387edd424f92a668e558902fb390f43fe4
Timothy Wu <timothytwu@ucsb.edu>
2025-04-28 14:36:48 -0700

    timothy - add fixtures for menuitemreviews

---
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -0,0 +1,39 @@
+const menuItemReviewFixtures = {
+    oneReview: {
+        "id": 5,
+        "itemId": 1,
+        "reviewerEmail": "cgaucho@ucsb.edu",
+        "stars": 3,
+        "dateReviewed": "2025-04-28T21:28:44.426",
+        "comments": "mac and cheese needs more cheese"
+      },
+    threeReviews: [
+        {
+          "id": 5,
+          "itemId": 1,
+          "reviewerEmail": "cgaucho@ucsb.edu",
+          "stars": 3,
+          "dateReviewed": "2025-04-28T21:28:44.426",
+          "comments": "mac and cheese needs more cheese"
+        },
+        {
+          "id": 6,
+          "itemId": 4,
+          "reviewerEmail": "cgaucho2@ucsb.edu",
+          "stars": 4,
+          "dateReviewed": "2025-04-28T22:28:44.426",
+          "comments": "salad was nice and fresh"
+        },
+        {
+          "id": 7,
+          "itemId": 8,
+          "reviewerEmail": "cgaucho3@ucsb.edu",
+          "stars": 2,
+          "dateReviewed": "2025-04-28T22:28:44.426",
+          "comments": "turkey was super dry "
+        }
+      ],
+  };
+  
+  export { menuItemReviewFixtures };
+  
