1f0f58706e64152ba496ed8cc86ff23e5bd7aa1c
Kenneth Tan <kennethtan@ucsb.edu>
2025-05-01 16:52:29 -0700

    kenneth - added placeholder for rec req

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,6 +26,8 @@ import PlaceholderIndexPage from "main/pages/Placeholder/PlaceholderIndexPage";
 import PlaceholderCreatePage from "main/pages/Placeholder/PlaceholderCreatePage";
 import PlaceholderEditPage from "main/pages/Placeholder/PlaceholderEditPage";
 
+
+
 import { hasRole, useCurrentUser } from "main/utils/currentUser";
 
 import "bootstrap/dist/css/bootstrap.css";
