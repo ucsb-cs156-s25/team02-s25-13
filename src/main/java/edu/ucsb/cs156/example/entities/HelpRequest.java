e65b8ece84ee0e0d375a16ccbed417f17b52fb5a
chengcxi <cxi@ucsb.edu>
2025-05-03 00:42:17 -0700

    fix

--- src/main/java/edu/ucsb/cs156/example/entities/HelpRequest.java
+++ src/main/java/edu/ucsb/cs156/example/entities/HelpRequest.java
@@ -14,6 +14,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public class HelpRequest {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String requesterEmail;
     private String teamId;
